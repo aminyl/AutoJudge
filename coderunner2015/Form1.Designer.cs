93a7f076e07f6b5de3a6d22f3928b8aeedb33d1e
aminyl <ellallopll@gmail.com>
2015-11-14 18:41:02 +0900

    first commit

---
+++ coderunner2015/Form1.Designer.cs
@@ -0,0 +1,229 @@
+﻿namespace coderunner2015
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// 必要なデザイナー変数です。
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 使用中のリソースをすべてクリーンアップします。
+        /// </summary>
+        /// <param name="disposing">マネージ リソースが破棄される場合 true、破棄されない場合は false です。</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows フォーム デザイナーで生成されたコード
+
+        /// <summary>
+        /// デザイナー サポートに必要なメソッドです。このメソッドの内容を
+        /// コード エディターで変更しないでください。
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label_SAA1 = new System.Windows.Forms.Label();
+            this.send = new System.Windows.Forms.Button();
+            this.textBox_QA1 = new System.Windows.Forms.TextBox();
+            this.textBox_GTA1 = new System.Windows.Forms.TextBox();
+            this.label_CHA1 = new System.Windows.Forms.Label();
+            this.tabControl1 = new System.Windows.Forms.TabControl();
+            this.tabPage_A = new System.Windows.Forms.TabPage();
+            this.tabPage_B = new System.Windows.Forms.TabPage();
+            this.tabPage_C = new System.Windows.Forms.TabPage();
+            this.tabPage_D = new System.Windows.Forms.TabPage();
+            this.tabPage_E = new System.Windows.Forms.TabPage();
+            this.tabPage_F = new System.Windows.Forms.TabPage();
+            this.tabPage_G = new System.Windows.Forms.TabPage();
+            this.tabPage_H = new System.Windows.Forms.TabPage();
+            this.tabControl1.SuspendLayout();
+            this.tabPage_A.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // label_SAA1
+            // 
+            this.label_SAA1.AutoSize = true;
+            this.label_SAA1.Location = new System.Drawing.Point(496, 143);
+            this.label_SAA1.Name = "label_SAA1";
+            this.label_SAA1.Size = new System.Drawing.Size(34, 12);
+            this.label_SAA1.TabIndex = 10;
+            this.label_SAA1.Text = "string";
+            this.label_SAA1.Click += new System.EventHandler(this.No_Click);
+            // 
+            // send
+            // 
+            this.send.Location = new System.Drawing.Point(523, 6);
+            this.send.Name = "send";
+            this.send.Size = new System.Drawing.Size(103, 79);
+            this.send.TabIndex = 4;
+            this.send.Text = "send";
+            this.send.UseVisualStyleBackColor = true;
+            this.send.Click += new System.EventHandler(this.send_Click);
+            // 
+            // textBox_QA1
+            // 
+            this.textBox_QA1.AcceptsReturn = true;
+            this.textBox_QA1.AcceptsTab = true;
+            this.textBox_QA1.AutoCompleteMode = System.Windows.Forms.AutoCompleteMode.Append;
+            this.textBox_QA1.Location = new System.Drawing.Point(20, 140);
+            this.textBox_QA1.Multiline = true;
+            this.textBox_QA1.Name = "textBox_QA1";
+            this.textBox_QA1.Size = new System.Drawing.Size(231, 112);
+            this.textBox_QA1.TabIndex = 11;
+            // 
+            // textBox_GTA1
+            // 
+            this.textBox_GTA1.AcceptsReturn = true;
+            this.textBox_GTA1.AcceptsTab = true;
+            this.textBox_GTA1.AutoCompleteMode = System.Windows.Forms.AutoCompleteMode.Append;
+            this.textBox_GTA1.Location = new System.Drawing.Point(257, 140);
+            this.textBox_GTA1.Multiline = true;
+            this.textBox_GTA1.Name = "textBox_GTA1";
+            this.textBox_GTA1.Size = new System.Drawing.Size(231, 86);
+            this.textBox_GTA1.TabIndex = 11;
+            // 
+            // label_CHA1
+            // 
+            this.label_CHA1.AutoSize = true;
+            this.label_CHA1.Location = new System.Drawing.Point(568, 143);
+            this.label_CHA1.Name = "label_CHA1";
+            this.label_CHA1.Size = new System.Drawing.Size(34, 12);
+            this.label_CHA1.TabIndex = 10;
+            this.label_CHA1.Text = "string";
+            this.label_CHA1.Click += new System.EventHandler(this.No_Click);
+            // 
+            // tabControl1
+            // 
+            this.tabControl1.Controls.Add(this.tabPage_A);
+            this.tabControl1.Controls.Add(this.tabPage_B);
+            this.tabControl1.Controls.Add(this.tabPage_C);
+            this.tabControl1.Controls.Add(this.tabPage_D);
+            this.tabControl1.Controls.Add(this.tabPage_E);
+            this.tabControl1.Controls.Add(this.tabPage_F);
+            this.tabControl1.Controls.Add(this.tabPage_G);
+            this.tabControl1.Controls.Add(this.tabPage_H);
+            this.tabControl1.Location = new System.Drawing.Point(21, 250);
+            this.tabControl1.Name = "tabControl1";
+            this.tabControl1.SelectedIndex = 0;
+            this.tabControl1.Size = new System.Drawing.Size(650, 462);
+            this.tabControl1.TabIndex = 12;
+            // 
+            // tabPage_A
+            // 
+            this.tabPage_A.Controls.Add(this.textBox_GTA1);
+            this.tabPage_A.Controls.Add(this.send);
+            this.tabPage_A.Controls.Add(this.label_SAA1);
+            this.tabPage_A.Controls.Add(this.textBox_QA1);
+            this.tabPage_A.Controls.Add(this.label_CHA1);
+            this.tabPage_A.Location = new System.Drawing.Point(4, 22);
+            this.tabPage_A.Name = "tabPage_A";
+            this.tabPage_A.Padding = new System.Windows.Forms.Padding(3);
+            this.tabPage_A.Size = new System.Drawing.Size(642, 436);
+            this.tabPage_A.TabIndex = 0;
+            this.tabPage_A.Text = "A";
+            this.tabPage_A.UseVisualStyleBackColor = true;
+            // 
+            // tabPage_B
+            // 
+            this.tabPage_B.Location = new System.Drawing.Point(4, 22);
+            this.tabPage_B.Name = "tabPage_B";
+            this.tabPage_B.Padding = new System.Windows.Forms.Padding(3);
+            this.tabPage_B.Size = new System.Drawing.Size(515, 332);
+            this.tabPage_B.TabIndex = 1;
+            this.tabPage_B.Text = "B";
+            this.tabPage_B.UseVisualStyleBackColor = true;
+            // 
+            // tabPage_C
+            // 
+            this.tabPage_C.Location = new System.Drawing.Point(4, 22);
+            this.tabPage_C.Name = "tabPage_C";
+            this.tabPage_C.Size = new System.Drawing.Size(515, 332);
+            this.tabPage_C.TabIndex = 2;
+            this.tabPage_C.Text = "C";
+            this.tabPage_C.UseVisualStyleBackColor = true;
+            // 
+            // tabPage_D
+            // 
+            this.tabPage_D.Location = new System.Drawing.Point(4, 22);
+            this.tabPage_D.Name = "tabPage_D";
+            this.tabPage_D.Size = new System.Drawing.Size(515, 332);
+            this.tabPage_D.TabIndex = 3;
+            this.tabPage_D.Text = "D";
+            this.tabPage_D.UseVisualStyleBackColor = true;
+            // 
+            // tabPage_E
+            // 
+            this.tabPage_E.Location = new System.Drawing.Point(4, 22);
+            this.tabPage_E.Name = "tabPage_E";
+            this.tabPage_E.Size = new System.Drawing.Size(515, 332);
+            this.tabPage_E.TabIndex = 4;
+            this.tabPage_E.Text = "E";
+            this.tabPage_E.UseVisualStyleBackColor = true;
+            // 
+            // tabPage_F
+            // 
+            this.tabPage_F.Location = new System.Drawing.Point(4, 22);
+            this.tabPage_F.Name = "tabPage_F";
+            this.tabPage_F.Size = new System.Drawing.Size(515, 332);
+            this.tabPage_F.TabIndex = 5;
+            this.tabPage_F.Text = "F";
+            this.tabPage_F.UseVisualStyleBackColor = true;
+            // 
+            // tabPage_G
+            // 
+            this.tabPage_G.Location = new System.Drawing.Point(4, 22);
+            this.tabPage_G.Name = "tabPage_G";
+            this.tabPage_G.Size = new System.Drawing.Size(515, 332);
+            this.tabPage_G.TabIndex = 6;
+            this.tabPage_G.Text = "G";
+            this.tabPage_G.UseVisualStyleBackColor = true;
+            // 
+            // tabPage_H
+            // 
+            this.tabPage_H.Location = new System.Drawing.Point(4, 22);
+            this.tabPage_H.Name = "tabPage_H";
+            this.tabPage_H.Size = new System.Drawing.Size(515, 332);
+            this.tabPage_H.TabIndex = 7;
+            this.tabPage_H.Text = "H";
+            this.tabPage_H.UseVisualStyleBackColor = true;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(668, 703);
+            this.Controls.Add(this.tabControl1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.tabControl1.ResumeLayout(false);
+            this.tabPage_A.ResumeLayout(false);
+            this.tabPage_A.PerformLayout();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label_SAA1;
+        private System.Windows.Forms.Button send;
+        private System.Windows.Forms.TextBox textBox_QA1;
+        private System.Windows.Forms.TextBox textBox_GTA1;
+        private System.Windows.Forms.Label label_CHA1;
+        private System.Windows.Forms.TabControl tabControl1;
+        private System.Windows.Forms.TabPage tabPage_A;
+        private System.Windows.Forms.TabPage tabPage_B;
+        private System.Windows.Forms.TabPage tabPage_C;
+        private System.Windows.Forms.TabPage tabPage_D;
+        private System.Windows.Forms.TabPage tabPage_E;
+        private System.Windows.Forms.TabPage tabPage_F;
+        private System.Windows.Forms.TabPage tabPage_G;
+        private System.Windows.Forms.TabPage tabPage_H;
+    }
+}
+
---
+++ coderunner2015/Form1.cs
@@ -0,0 +1,171 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Diagnostics;
+using System.Collections;
+using System.Xml.Serialization;
+using System.IO;
+
+namespace coderunner2015
+{
+    public partial class Form1 : Form
+    {
+        System.Diagnostics.Process p;
+        string result;
+        string command;
+        string[] problemStrs = { "A", "B", "C", "D", "E", "F", "G", "H", "I", "J" };
+        string answerFilePath = @"C:\Users\blackhat\GoogleDrive\ProgrammingContest\ruby\vimcodes\";
+        string fileType = ".rb";
+
+        public Form1()
+        {
+            InitializeComponent();
+            init();
+        }
+
+        private void init()
+        {
+            makeBtn();
+            makeTxtBox();
+        }
+
+        private void makeBtn()
+        {
+            int tabHeight = 23, tabWidth = 48, tabOffset = 1, tabMergLeft = 5, tabMergUp = 10, tabNum = 8;
+            this.btnTabs = new System.Windows.Forms.Button[tabNum];
+            //ボタンコントロールのインスタンス作成し、プロパティを設定する
+            this.SuspendLayout();
+            for (int i = 0; i < this.btnTabs.Length; i++)
+            {
+                //インスタンス作成
+                this.btnTabs[i] = new System.Windows.Forms.Button();
+                //プロパティ設定
+                this.btnTabs[i].Name = "btn" + problemStrs[i];
+                this.btnTabs[i].Text = problemStrs[i];
+                this.btnTabs[i].Size = new Size(tabWidth, tabHeight);
+                this.btnTabs[i].Location = new Point(i * tabWidth - i * tabOffset + tabMergLeft, tabMergUp);
+                this.btnTabs[i].FlatStyle = FlatStyle.Popup;
+                //イベントハンドラに関連付け
+                this.btnTabs[i].Click +=
+                    new EventHandler(this.btnTabs_Click);
+            }
+            //フォームにコントロールを追加
+            this.Controls.AddRange(this.btnTabs);
+            this.ResumeLayout(false);
+        }
+        private System.Windows.Forms.Button[] btnTabs;
+        private void btnTabs_Click(object sender, EventArgs e)
+        {
+            MessageBox.Show(((System.Windows.Forms.Button)sender).Name);
+        }
+
+        private void makeTxtBox()
+        {
+            int txtbxHeight = 50, txtbxWidth = 48, txtbxOffset = 1, txtbxMergLeft = 5, txtbxMergUp = 50, txtbxNum = 5;
+            this.txtbxAnswers = new System.Windows.Forms.TextBox[txtbxNum];
+            this.SuspendLayout();
+            for (int i = 0; i < this.txtbxAnswers.Length; i++)
+            {
+                //インスタンス作成
+                this.txtbxAnswers[i] = new System.Windows.Forms.TextBox();
+                //プロパティ設定
+                this.txtbxAnswers[i].Name = "GroundTruth" + problemStrs[i];
+                this.txtbxAnswers[i].Text = "";
+                this.txtbxAnswers[i].Size = new Size(txtbxWidth, txtbxHeight);
+                this.txtbxAnswers[i].Location = new Point(txtbxMergLeft, i *txtbxHeight - i * txtbxOffset + txtbxMergUp);
+                this.txtbxAnswers[i].AcceptsReturn = true;
+                this.txtbxAnswers[i].Multiline = true;
+                //イベントハンドラに関連付け
+                this.txtbxAnswers[i].Click +=
+                    new EventHandler(this.txtbxAnswers_Click);
+            }
+            //フォームにコントロールを追加
+            this.Controls.AddRange(this.txtbxAnswers);
+            this.ResumeLayout(false);
+        }
+        private System.Windows.Forms.TextBox[] txtbxAnswers;
+        private void txtbxAnswers_Click(object sender, EventArgs e)
+        {
+            MessageBox.Show(((System.Windows.Forms.Button)sender).Name);
+        }
+
+        private void send_Click(object sender, EventArgs e)
+        {
+            label_SAA1.Text = excuse("A");
+            if (label_SAA1.Text == textBox_GTA1.Text)
+                label_CHA1.Text = "AC";
+            else
+                label_CHA1.Text = "WA";
+        }
+
+        private string excuse(string s)
+        {
+            p = new System.Diagnostics.Process();
+            p.StartInfo.FileName = "ruby.exe";
+            p.StartInfo.CreateNoWindow = true;
+            p.StartInfo.UseShellExecute = false;
+            p.StartInfo.RedirectStandardInput = true;
+            p.StartInfo.RedirectStandardOutput = true;
+            p.StartInfo.Arguments = answerFilePath + s + fileType;
+            // cmd.exe を起動します。
+            p.Start();
+            // cmd.exe で実行するコマンドを取得します。
+            command = textBox_QA1.Text;
+            // 標準入出力からリードライトするためのスレッドを起動します。
+            this.StartThread();
+            // cmd.exe が終了するのを待ちます。
+            p.WaitForExit();
+            // 結果を　textBox2 へ書き出します。
+            return result;
+        }
+
+        public void StartThread()
+        {
+            System.Threading.ThreadStart readThread = new System.Threading.ThreadStart(ReadThread);
+            System.Threading.ThreadStart writeThread = new System.Threading.ThreadStart(WriteThread);
+            System.Threading.Thread rThread = new System.Threading.Thread(readThread);
+            System.Threading.Thread wThread = new System.Threading.Thread(writeThread);
+            rThread.Name = "ReadThread";
+            wThread.Name = "WriteThread";
+            rThread.Start();       //starting the read thread
+            wThread.Start();       //starting the write thread
+            wThread.Join();
+            rThread.Join();
+        }
+
+        private void ReadThread()
+        {
+            result = p.StandardOutput.ReadToEnd();
+        }
+
+        private void WriteThread()
+        {
+            p.StandardInput.WriteLine(command);
+            p.StandardInput.WriteLine("exit");
+        }
+
+
+        private string getArgument_result()
+        {
+            //return @"C:\Users\blackhat\GoogleDrive\ProgrammingContest\ruby\vimcodes\A.rb" + " < " + @"C:\Users\blackhat\Desktop\testcase.txt";
+            return "C:\\Users\\blackhat\\GoogleDrive\\ProgrammingContest\\ruby\\vimcodes\\A.rb < C:\\Users\\blackhat\\Desktop\\testcase.txt";
+        }
+
+        private string startPSI(ProcessStartInfo psi)
+        {
+            Process p = Process.Start(psi); // アプリの実行開始
+            string output = p.StandardOutput.ReadToEnd(); // 標準出力の読み取り
+            return output.Replace("\r\r\n", "\n"); // 改行コードの修正
+        }
+
+        private void No_Click(object sender, EventArgs e)
+        {
+        }
+    }
+}
